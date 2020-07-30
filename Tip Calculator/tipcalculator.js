3aa18a4f77d6864e2f6c3810f91ad5aca6426ad5
Kennie6908 <60665094+Kennie6908@users.noreply.github.com>
2020-07-30 17:02:43 -0400

    upload tip calculator

---
+++ Tip Calculator/tipcalculator.js
@@ -0,0 +1,17 @@
+var num; 
+var bill;
+var total;
+
+window.onload = function() {
+	document.getElementById('slider').addEventListener("input", function(){
+	num = parseFloat(document.getElementById("slider").value); 
+	document.getElementById("label").innerHTML = (num + "%");
+	bill = parseFloat(document.getElementById("bill").value);
+    var tip = (num / 100) * bill;
+	tip = tip.toFixed(2); 
+    total = 1 * tip + bill; 
+	total = total.toFixed(2);
+	document.getElementById("tip").value = tip;
+	document.getElementById("total").value = total;
+	});
+};
